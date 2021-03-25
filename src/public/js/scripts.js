3c67dbd9beaa6beb68e6494144486eec28492cd6
Martin Fernandez <dosunounodev@gmail.com>
2021-03-25 11:34:14 -0300

    menu y maqueta de secciones y titulos h2 y h3

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,20 +1,27 @@
-const callback = (entries) => {
+const hero = document.getElementById('header__hero')
+const navbar = document.getElementById('header__navbar')
+const menubtn = document.getElementById('menu-icon-container')
+const menucontainer = document.getElementById('menu__container')
+
+const observer = new IntersectionObserver(observarEntries)
+
+function observarEntries (entries) {
   if(entries[0].isIntersecting) {
     navbar.classList.remove('navbar-fix')
   } else {
     navbar.classList.add('navbar-fix')
   }
 }
-const hero = document.getElementById('header__hero')
-const navbar = document.getElementById('header__navbar')
-const observer = new IntersectionObserver(callback)
-observer.observe(hero)
 
-const menubtn = document.getElementById('menu-icon-container')
-const menucontainer = document.getElementById('menu__container')
+observer.observe(hero)
 
 menubtn.addEventListener('click', (e) => {
   menubtn.firstElementChild.classList.toggle('menu-icon--active')
   menucontainer.classList.toggle('menu__container--hide')
 })
 
+menucontainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('menu__item')) {
+    menucontainer.classList.toggle('menu__container--hide')
+  }
+})
