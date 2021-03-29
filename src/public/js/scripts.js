91a1ed81c8134a33112cf73d01f92c6bca4d2e95
Martin Fernandez <dosunounodev@gmail.com>
2021-03-29 18:40:02 -0300

    avance en secciones: about, comentarios, banner

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,24 +1,14 @@
-const hero = document.getElementById('header__hero')
-const navbar = document.getElementById('header__navbar')
 const menubtn = document.getElementById('menu-icon-container')
 const menucontainer = document.getElementById('menu__container')
 
-const observer = new IntersectionObserver(observarEntries)
-
-function observarEntries (entries) {
-  if(entries[0].isIntersecting) {
-    navbar.classList.remove('navbar-fix')
-  } else {
-    navbar.classList.add('navbar-fix')
-  }
-}
+const coment1 = document.getElementById('comentario-01')
+const coment2 = document.getElementById('comentario-02')
+const coment3 = document.getElementById('comentario-03')
 
 function toggleMenu() {
   menubtn.firstElementChild.classList.toggle('menu-icon--active')
   menucontainer.classList.toggle('menu__container--hide')
 }
-
-observer.observe(hero)
 
 menubtn.addEventListener('click', (e) => {
   toggleMenu()
@@ -29,3 +19,45 @@ menucontainer.addEventListener('click', (e) => {
     toggleMenu()
   }
 })
+
+setInterval(() => {
+  changeComment()
+}, 5000);
+
+function changeComment() {
+  if(coment1.classList.contains('comentario__on')){
+    coment1.classList.toggle('comentario__hide')
+    coment1.classList.toggle('comentario__on')
+    coment2.classList.toggle('comentario__hide')
+    coment2.classList.toggle('comentario__on')
+
+  } else if(coment2.classList.contains('comentario__on')){
+    coment2.classList.toggle('comentario__hide')
+    coment2.classList.toggle('comentario__on')
+    coment3.classList.toggle('comentario__hide')
+    coment3.classList.toggle('comentario__on')
+  
+  } else if(coment3.classList.contains('comentario__on')){
+  coment3.classList.toggle('comentario__hide')
+  coment3.classList.toggle('comentario__on')
+  coment1.classList.toggle('comentario__hide')
+  coment1.classList.toggle('comentario__on')
+  }
+}
+
+
+
+// setTimeout(() => {
+//   coment1.classList.toggle('comentario__hide')
+//   coment2.classList.toggle('comentario__hide')
+// }, 10000);
+
+// setTimeout(() => {
+//   coment2.classList.toggle('comentario__hide')
+//   coment3.classList.toggle('comentario__hide')
+// }, 20000);
+
+// setTimeout(() => {
+//   coment3.classList.toggle('comentario__hide')
+//   coment1.classList.toggle('comentario__hide')
+// }, 30000);
